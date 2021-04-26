5ee03590269d6faa04567653ef8306ca4fdf9acf
ddant <ddant@DESKTOP-6976.6973>
2021-04-26 15:47:05 +0200

    Versione iniziale ver.01.00

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.d4n73.provagit {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.d4n73.provagit to javafx.fxml;
+    exports com.d4n73.provagit;
+}
+*/
